3ea2c56c8bc4e0be8074752758241bfb79c6bbe6
MohamedAbdElwahabOka <boka22986@gmail.com>
2024-05-21 00:57:56 +0300

    refactor: Update New Patient registration form layout, add logo, and modify input fields and buttons
    The New Patient registration form has been refactored to improve the layout and user experience. A logo has been added to enhance branding. Additionally, the input fields and buttons have been modified for better usability.

--- app/NewPatient/[recepRegNum]/page.jsx
+++ app/NewPatient/[recepRegNum]/page.jsx
@@ -9,7 +9,7 @@ export default function page({params}){
           reg={params.recepRegNum}
           className="w-64 bg-gray-800 text-white px-4 py-8" />
           <div className="flex-grow bg-gray-100 p-8">
-            <New_patient reg={params.recepRegNum}/>
+            <New_patient ReceptionistsRegNum={params.recepRegNum}/>
           </div>
         </div>
           
