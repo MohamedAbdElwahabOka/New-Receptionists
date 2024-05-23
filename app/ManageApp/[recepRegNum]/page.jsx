2e0125d2ec6af6dbcd0cca98c7654584ac83eef7
MohamedAbdElwahabOka <boka22986@gmail.com>
2024-05-24 02:21:37 +0300

    "Added state and effect to page component to fetch appointments, implemented loading state, and passed appointments data to Manage_app component"

--- app/ManageApp/[recepRegNum]/page.jsx
+++ app/ManageApp/[recepRegNum]/page.jsx
@@ -1,17 +1,76 @@
+'use client'
 import Manage_app from '../_components/Manage_app';
 import Sidebar from '../../_components/Sidebar'
+import Swal from 'sweetalert2'
+import React, { useEffect, useState } from 'react'
+import AppointmentsAPI from '@/app/_Utils/AppointmentsAPI';
 export default function page({params}){
+
   console.log(params.recepRegNum)
+  const [Appointments, setAppointments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
+
+  
+  useEffect(() => {
+    getAppointments_();
+  },[])
+
+  const getAppointments_ = () => {
+    Swal.fire({
+      title: 'Loading...',
+      html: '<img class="my-loading-gif" src="/heart_loading.gif" alt="Loading..." />',
+      showConfirmButton: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      allowEnterKey: false,
+      customClass: {
+        popup: 'my-custom-popup'
+      }
+    });
+    AppointmentsAPI.getAppointments()
+    .then(res => {
+      console.log(res.data.data);
+      setAppointments(res.data.data);
+      setIsLoading(false);
+      Swal.close();
+      
+    })
+    .catch(err => {
+      console.error(err);
+      setIsLoading(false);
+      Swal.close();
+      // Handle error appropriately
+    });
+
+  }
+
+
+
     return(
-        <div>
-    <div className="flex h-screen">  
-      <Sidebar 
-      reg={params.recepRegNum}
-      className="w-64 bg-gray-800 text-white px-4 py-8" />
-      <div className="flex-grow bg-gray-100 p-8">
-      <Manage_app/>
+    <div>
+      <div className="flex h-screen">  
+        <Sidebar 
+        reg={params.recepRegNum}
+        className="w-64 bg-gray-800 text-white px-4 py-8" />
+        <div className="flex-grow bg-gray-100 p-8">
+          {
+        isLoading ?(
+          
+          <div className="flex items-center justify-center min-h-screen">
+          <h1></h1></div>)
+
+          
+          :(<Manage_app Appointments={Appointments}/>)
+            
+          }
+
+
+        
+
+
+        </div>
       </div>
-    </div>
     </div>
     )
 }
